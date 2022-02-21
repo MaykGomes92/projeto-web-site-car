89228387a47175c255f6719cefa8b3a18283f249
MaykGomes92 <maykcontato98@hotmail.com>
2022-02-21 00:06:08 -0300

    segundo commit animacao js

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,50 +1,82 @@
 import React from "react";
 
 const Slide = () => {
-  const distancia = React.useRef(null);
+  // const [ativo, setAtivo] = React.useState(false)
 
-  function clickLeft(e) {
-    distancia.current.scrollLeft -= distancia.current.offsetWidth;
-  }
-  function clickRight(e) {
-    distancia.current.scrollLeft += distancia.current.offsetWidth;
-  }
+  // function clickLeft(e) {
+  //   setAtivo(!ativo)
+  //   distancia.current.scrollLeft -= distancia.current.offsetWidth;
+  //   if(ativo){
+  //     teste()
+  //   }
+  // }
+
+  // let imgCar1 = document.querySelector('.car1')
+  // let circle2 = document.querySelector('.circle2')
+  // let imgCar2 = document.querySelector('.car2')
+
+  // function clickRight(e) {
+  //   setAtivo(!ativo)
+  //   distancia.current.scrollLeft += distancia.current.offsetWidth;
+  //   if(ativo){
+  //     teste()
+  //   }
+  // }
+
+  let car2 = document.querySelector('.car2')
+  const distancia = React.useRef(null);
+  React.useEffect(
+    (e) => {
+      // try {
+      //   teste()
+      // } catch{
+      //   console.log('')
+      // }
+      setInterval(() => {
+        distancia.current.scrollLeft += 1920;
+      }, 10000);
+      setInterval(() => {
+        distancia.current.scrollLeft = 0;
+      }, 30000);
+    },
+    [distancia]
+  );
 
   return (
     <div className="containerTotal">
-      <button id="left" onClick={clickLeft}>
-        <img src="./img/seta-neon-azul.png" alt="seta" />
-      </button>
-      <button id="rigth" onClick={clickRight}>
-        {" "}
-        <img src="./img/seta-neon-azul.png" alt="seta" />
-      </button>
       <div className="slides" ref={distancia}>
         <div className="slide1 slide-First">
           <div>
             <h1>IMAGINE ?</h1>
-          <span id="circle"></span>
+            <span id="circle"></span>
             <p>
-                Image ter uma máquina dessas. Um Mazda RX-Vision!<br />
-                Contendo 1.200 cv, motor v8 e fazendo de 0 a 200km/h em 3 segundos. 
-
+              Image ter uma máquina dessas. Um Mazda RX-Vision!
+              <br />
+              Contendo 1.200 cv, motor v8 e fazendo de 0 a 200km/h em 3
+              segundos.
             </p>
           </div>
           <div>
             <h1>Mazda RX-Vision GT3</h1>
-            <img src="./img/rx-car-1-pn.png"alt="carro" />
-            <img id="carSom" src="./img/rx-car-1-pn.png"alt="carro" />
+            <img className="car1" src="./img/rx-car-1-pn.png" alt="carro" />
+            <img id="carSom" src="./img/rx-car-1-pn.png" alt="carro" />
           </div>
         </div>
 
         <div id="second" className="slide1 slide-Second">
-          <h1>IMAGINED ??</h1>
-          <p>
-            TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE
-            TESTE TESTE TESTE TESTETESTE TESTE TESTE TESTE TESTE TESTE TESTE
-            TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE
-          </p>
-          <img src="./img/rx-car-back.jpg" alt="carro" />
+          <div>
+            <h1>IMAGINED ??</h1>
+            <span className="circle2"></span>
+            <p>
+              TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE
+              TESTE TESTE TESTE TESTETESTE TESTE TESTE TESTE TESTE TESTE TESTE
+              TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE
+            </p>
+          </div>
+          <div>
+            <img className="car2" src="./img/rx-car-1-pn.png" alt="carro" />
+            <img id="carSom2" src="./img/rx-car-1-pn.png" alt="carro" />
+          </div>
         </div>
         <div className="slide1 slide-Three">
           <div>
